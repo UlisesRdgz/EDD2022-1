06bd68c7a0c21afcdd22c192fb735a7da527989a
Zuri Uribe <zuriug@ciencias.unam.mx>
2021-10-08 18:53:33 -0500

    Se añaden primeros métodos

--- Practica02RodriguezUlisesUribeZurisadai/src/fciencias/edatos/practica02/DoubleLinkedList.java
+++ Practica02RodriguezUlisesUribeZurisadai/src/fciencias/edatos/practica02/DoubleLinkedList.java
@@ -1,5 +1,7 @@
 package fciencias.edatos.practica02;
 
+import fciencias.edatos.practica01.TDAList;
+
 /**
 * Double Linked Lists.
 * @author Rodríguez García Ulises.
@@ -8,8 +10,75 @@ package fciencias.edatos.practica02;
 * @since Estructuras de datos 2022-1. Prática 2.
 */
 
-public class DoubleLinkedList<T>{
+public class DoubleLinkedList<T> implements TDAList<T>{
 
+	private class Node{
+
+		public T element;
+
+		public Node next; //siguiente
+
+		public Node prev; //anterior
+
+		/**
+		 * Crea un nuevo nodo
+		 * @param element elemento que almacena el nodo 
+		 */
+		public Node(T element){
+			this.element = element;
+		} 
+
+		/**
+		 * Permite cambiar al siguiente nodo
+		 * @param newNode
+		 */
+		public void setNext(Node newNode){
+			this.next = newNode;
+		}
+
+		/**
+		 * Permite cambiar al nodo anterior
+		 * @param newNode
+		 */
+		public void setPrev(Node newNode){
+			this.prev = newNode;
+		}
+
+		/**
+		 * Accede a la información del nodo
+		 * @return element 
+		 */
+		public T getElement(){
+			return element;
+		}
+
+		/**
+		 * Accede al nodo siguiente
+		 * @return next
+		 */
+		public Node getNext(){
+			return next;
+		}
+
+		/**
+		 * Accede al nodo anterior
+		 * @return prev
+		 */
+		public Node getPrev(){
+			return prev;
+		}
+
+		
+	}
+	/** Cabeza de la lista */
+	private Node head;
+
+	/** Cola de la lista */
+	private Node tail;
+
+	/** Longitud de la lista */
+	private int size;
+	
 	/**
 	 * Inserta un nuevo elemento <i>e</i> en la posición <i>i</i>.
 	 * @param i la posición donde agregar el elemento.
@@ -19,11 +88,16 @@ public class DoubleLinkedList<T>{
     @Override 
 	public void add(int i, T e) throws IndexOutOfBoundsException;
 
+
 	/**
 	 * Limpia la lista. Elimina todos los elementos.
 	 */
     @Override 
-	public void clear();
+	public void clear(){
+		head = null;
+		tail = null;
+		size = 0;
+	}
 
 	/**
 	 * Verifica si un elemento está contenido en la lista.
@@ -46,7 +120,12 @@ public class DoubleLinkedList<T>{
 	 * @return true si la lista no contiene elementos, false en otro caso.
 	 */
     @Override 
-	public boolean isEmpty();
+	public boolean isEmpty(){
+		if (size != 0) 
+			return false;
+		return true;
+	}
+
 
 	/**
 	 * Elimina el elemento en la posición <i>i</i>.
@@ -62,7 +141,9 @@ public class DoubleLinkedList<T>{
 	 * @return la cantidad de elementos contenidos.
 	 */
     @Override 
-	public int size();
+	public int size(){
+		return size;
+	}
 
 	/**
 	 * Revierte los elementos de la lista. Esto es, da la reversa de la lista.

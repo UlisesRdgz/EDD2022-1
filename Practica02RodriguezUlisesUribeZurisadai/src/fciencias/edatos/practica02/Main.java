40ad42f72ea780a7f8c4baafa99a9d5aa60483a0
U <ulises.rdgz@ciencias.unam.mx>
2021-10-14 16:32:20 -0500

    Modificaciones segunda parte del menú
    Se Termino la segunda parte del menú aún hacen falta algunos cambios, checar el método sleep y clearScreen.

--- Practica02RodriguezUlisesUribeZurisadai/src/fciencias/edatos/practica02/Main.java
+++ Practica02RodriguezUlisesUribeZurisadai/src/fciencias/edatos/practica02/Main.java
@@ -3,26 +3,52 @@ package fciencias.edatos.practica02;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }  
+
+    public static void sleep(){
+        try {
+            //Ponemos a "Dormir" el programa durante los ms que queremos
+            Thread.sleep(1*1200);
+         } catch (Exception e) {
+            System.out.print(e);
+         }
+    }
+
+    public static void sleep1(){
+        try {
+            //Ponemos a "Dormir" el programa durante los ms que queremos
+            Thread.sleep(1*1800);
+         } catch (Exception e) {
+            System.out.print(e);
+         }
+    }
+
     public static void main(String[] args) {
         DoubleLinkedList<String> lista = new DoubleLinkedList<>();
 
     Scanner sc = new Scanner(System.in);
 
     do{
-        System.out.println("Menú \n" +
-                           "1)  Agregar una cadena \n" +
-                           "2)  Elimina una cadena \n" +
-                           "3)  Limpiar \n" +
-                           "4)  Verificar si hay algún elemento \n" +
-                           "5)  Obtener un elemento \n" +
-                           "6)  Verificar si está vacía\n" +
-                           "7)  Longitud \n" +
-                           "8)  Reversa \n" +
-                           "9)  Cortar \n" +
-                           "10) Mostrar \n" +
-                           "11) Salir \n");
+        System.out.println(" Menú \n" +
+                           " 1)  Agregar una cadena \n" +
+                           " 2)  Elimina una cadena \n" +
+                           " 3)  Limpiar \n" +
+                           " 4)  Verificar si hay algún elemento \n" +
+                           " 5)  Obtener un elemento \n" +
+                           " 6)  Verificar si está vacía\n" +
+                           " 7)  Longitud \n" +
+                           " 8)  Reversa \n" +
+                           " 9)  Cortar \n" +
+                           " 10) Mostrar \n" +
+                           " 11) Salir \n");
                  
         int opcion = sc.nextInt();
+        System.out.println("");
+        clearScreen();
         
         switch (opcion) {
             case 1:
@@ -31,16 +57,20 @@ public class Main {
                     sc.nextLine();
                     String s = sc.nextLine();
                     lista.add(0, s);
-                    System.out.println("Se agregó correctamente \n");
+                    System.out.println("Se agregó correctamente en la posición 0.");
+                    sleep();
+                    clearScreen();
                     break;
                 }else{
-                    System.out.println("Coloca la posición de la cadena que deseas agregar (0 - " + lista.size() + "):\n ");
+                    System.out.println("Coloca la posición de la cadena que deseas agregar (0 - " + lista.size() + "):");
                     int i = sc.nextInt();
                     System.out.println("Escribe el elemento que deseas agregar: ");
                     sc.nextLine();
                     String s = sc.nextLine();
                     lista.add(i, s);
-                    System.out.println("Se agregó correctamente \n");
+                    System.out.println("Se agregó correctamente en la posición " + i + ".");
+                    sleep();
+                    clearScreen();
                     break;
                 }
             
@@ -69,10 +99,63 @@ public class Main {
                 System.out.println("Coloca el elemento que deseas obtener: ");
                 i= sc.nextInt();
                 sc.nextLine();            
-                System.out.println(lista.get(i));     
+                System.out.println(lista.get(i));  
+                
+            case 6:
+                if(lista.isEmpty())
+                    System.out.println("La lista esta vacia");
+                
+                else
+                    System.out.println("La lista no esta vacia");
+                
+                sleep1();
+                clearScreen();
+                break;
+            
+            case 7:
+                System.out.println("La longitud de la lista es: " + lista.size());
+                sleep1();
+                clearScreen();
+                break;
+
+            case 8:
+                System.out.println("La reversa de la lista es: \n");   
+                lista.revert();
+                System.out.println(lista);
+
+                sleep1();
+                clearScreen();
+                break;
+
+            case 9:
+                System.out.println("¿Mitad izquierda o mitad derecha?");
+                sc.nextLine();
+                s = sc.nextLine();
+
+                if(s=="true")
+                    lista.cut(true);
+        
+                else
+                    lista.cut(false);
+
+                System.out.println("Lista cortada: ");
+                System.out.println(lista);
+                sleep1();
+                clearScreen();
+                break;
+
+            case 10:
+                System.out.println("Lista: ");          
+                System.out.println(lista);
+                sleep1();
+                clearScreen(); 
+                break;
+
+            case 11:
+                return;
 
             default:
-                System.out.println("Opción inválida :c ");
+                System.out.println(" Opción inválida :c ");
                 break;
         }
 

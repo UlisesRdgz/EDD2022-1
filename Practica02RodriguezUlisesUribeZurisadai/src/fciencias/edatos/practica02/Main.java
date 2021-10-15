d73a53bca26b956f053e25b43f1e11d0764b9ce9
Zuri Uribe <zuriug@ciencias.unam.mx>
2021-10-15 12:50:10 -0500

    Primera parte del menú

--- Practica02RodriguezUlisesUribeZurisadai/src/fciencias/edatos/practica02/Main.java
+++ Practica02RodriguezUlisesUribeZurisadai/src/fciencias/edatos/practica02/Main.java
@@ -26,50 +26,82 @@ public class Main {
         
         switch (opcion) {
             case 1:
+            int i;
+            String s;
+            
+            if (lista.isEmpty()) {
+                System.out.println("\nEscribe el elemento que deseas agregar: ");
+                sc.nextLine();
+                s = sc.nextLine();
+                lista.add(0, s);
+                System.out.println("\nSe agregó correctamente en la posición 0.");
+                break;
+            }else{
+                do{
+                    System.out.println("\nColoca la posición de la cadena que deseas agregar (0 - " + lista.size() + "):");
+                    i = sc.nextInt();
+                }while(i > lista.size());
+                System.out.println("\nEscribe el elemento que deseas agregar: ");
+                sc.nextLine();
+                s = sc.nextLine();
+                lista.add(i, s);
+                System.out.println("\nSe agregó correctamente en la posición " + i + ".");
+                break;
+            }
+        
+            case 2:
+                int size = lista.size()-1;
                 if (lista.isEmpty()) {
-                    System.out.println("Escribe el elemento que deseas agregar: ");
-                    sc.nextLine();
-                    String s = sc.nextLine();
-                    lista.add(0, s);
-                    System.out.println("Se agregó correctamente \n");
+                    System.out.println("\nLa lista esta vacia");
+                    break;
+                }else if(size == 0){
+                    System.out.println("\nSe elimino el único elemento en la lista: " + lista.remove(0) + ".");
                     break;
                 }else{
-                    System.out.println("Coloca la posición de la cadena que deseas agregar (0 - " + lista.size() + "):\n ");
-                    int i = sc.nextInt();
-                    System.out.println("Escribe el elemento que deseas agregar: ");
-                    sc.nextLine();
-                    String s = sc.nextLine();
-                    lista.add(i, s);
-                    System.out.println("Se agregó correctamente \n");
+                    do{
+                        System.out.println("\nColoca la posición de la cadena que deseas eliminar (0 - " + size + "):");
+                        i = sc.nextInt();
+                    }while(i > size);
+                    System.out.println("\nSe elimino correctamente la posición " + i + " con el elemento : " + lista.remove(i) + ".");
                     break;
                 }
-            
-            case 2:
-                System.out.println("Coloca la posición de la cadena que deseas eliminar: ");
-                int i= sc.nextInt();
-                sc.nextLine();
-                lista.remove(i);
-                System.out.println("Se eliminó correctamente la cadena");
-                break;
 
             case 3:
-                System.out.println("La lista se limpiará en un momento");   
-                lista.clear();
-                System.out.println("Se limpió correctamente ");
+                if (lista.isEmpty()){ 
+                    System.out.println("\nLa lista ya esta vacia.");
+                }else{
+                    lista.clear();
+                    System.out.println("\nLa lista se limpió correctamente.");
+                }
                 break;
 
-            case 4:
-                System.out.println("Coloca el elemento que deseas verificar: ");
-                i= sc.nextInt();
-                sc.nextLine();
-                String s= sc.nextLine();                
-                System.out.println(lista.contains(s));
+                case 4:
+                if (lista.isEmpty()) {
+                    System.out.println("\nNo hay elementos en la lista.");
+                }else{
+                    System.out.println("\nColoca el elemento que deseas verificar.");
+                    s = sc.next();
+                    if(lista.contains(s))
+                        System.out.println("\nEl elemento se encuentra dentro de la lista.");
+                    else
+                        System.out.println("\nEl elemento no se encuentra.");
+                    
+                }
+                break;
 
             case 5:
-                System.out.println("Coloca el elemento que deseas obtener: ");
-                i= sc.nextInt();
-                sc.nextLine();            
-                System.out.println(lista.get(i));     
+                size = lista.size()-1;
+                if (lista.isEmpty()) {
+                    System.out.println("\nNo hay ningun elemento, la lista esta vacia");
+                }else if(lista.size() == 1){
+                    System.out.println("\nEl único elemento en la lista es: " + lista.get(0) + ".");
+                }else{
+                    System.out.println("\nColoca la posición del elemento que deseas obtener: (0 - " + size + ").");
+                    i= sc.nextInt(); 
+                    System.out.println("\nEl elemento en la posición " + i +" es: " + lista.get(i) + ".");
+                }
+                
+                break;  
 
             default:
                 System.out.println("Opción inválida :c ");

c5eaa64fe9afb15344c34ac5618cae636b55b90b
U <ulises.rdgz@ciencias.unam.mx>
2021-11-13 22:33:06 -0600

    Subimos QuickSort y mitad de Merge

--- Prácticas/Practica04RodríguezUlisesUribeZurisadai/src/fciencias/edatos/practica04/Sort.java
+++ Prácticas/Practica04RodríguezUlisesUribeZurisadai/src/fciencias/edatos/practica04/Sort.java
@@ -1,6 +1,7 @@
 package fciencias.edatos.practica04;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Implementación de métodos
@@ -13,25 +14,131 @@ public class Sort{
 
     /**
      * Intercambia dos elementos de posición en una misma colección
-     * @param arreglo arreglo que cambia la posición de elementos dados.
+     * @param arr arreglo que cambia la posición de elementos dados.
      * @param i índice del primer elemento 
      * @param j índice del segundo elemento
      */
-    private static void swap(int[] arreglo, int i, int j) {
-        int temp = arreglo[i];
-        arreglo[i] = arreglo[j];
-        arreglo[j] = temp;
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    public static void quickSort() {
+    public static void quicksort(int[] arr){
+        quicksort(arr, 0, arr.length-1);
+    }
         
+
+    public static void quicksort(int[] arr, int lo, int hi) {
+        if(hi <= lo)
+            return;
+        int j = partition(arr, lo, hi);
+        quicksort(arr, lo, j-1);
+        quicksort(arr, j+1, hi);
     }
 
-    public static void mergeSort() {
-        
+    public static int partition (int arr[], int lo, int hi){
+        int i = lo;
+        int j = hi+1;
+        int piv = arr[lo];
+
+        while (true) {
+            while (arr[++i] < piv){
+                if (i == hi)
+                    break;
+            }
+            while(piv < arr[--j]){
+                if (j == lo)
+                    break;
+            }
+            if(i >= j)
+                break;
+
+            swap(arr, i, j);
+        }
+        swap(arr, lo, j);
+        return j;
     }
+
+	/**
+	 * Ordena un arreglo de forma ascendente con merge sort.
+	 * @param arr el arreglo a ordenar.
+	 */
+	public static void mergeSort(int[] arr){
+		mergeSort(arr, 0, arr.length-1);
+	}
+
+	/**
+	 * Auxiliar de mergeSort para dividir y mezclar.
+	 * @param arr el arreglo con los elementos a dividir y mezclar.
+	 * @param lo el índice de inicio a modificación.
+	 * @param hi el índice del último elemento a modificación.
+	 */
+	private static void mergeSort(int[] arr, int lo, int hi){
+		// Cuando ya esta ordenado el fragmento de lo hasta hi
+		if(hi <= lo)
+			return;
+
+		// La mitad del corte del arreglo
+		int mid = lo + (hi-lo) / 2;
+
+		mergeSort(arr, lo, mid);
+		mergeSort(arr, mid+1, hi);
+
+		merge(arr, lo, mid, hi);
+	}
+
+	/**
+	 * Mezcla dos arreglos, ordenando de menor a mayor.
+	 * @param arr el arreglo con los elementos a modificar.
+	 * @param lo el inicio de la primera mitad.
+	 * @param mid el índice de la mitad del subarreglo.
+	 * @param hi el índice del último elemento.
+	 */
+	private static void merge(int[] arr, int lo, int mid, int hi){
+		int i = 0, j = 0;
+        //Creamos un dos arreglos auxiliares
+        int aux1[] = new int[mid - lo + 1];
+        int aux2[] = new int[hi - mid];
+		
+        for (int k = 0; k < aux1.length; k++)
+            aux1[k] = arr[lo + k];
+        for (int k = 0; k < aux2.length; k++)
+            aux2[k] = arr[mid + k + 1];
+
+		for(int k = lo ; k <= hi; k++){
+		}
+	}
 
     public int find(int[] arr, int e){
         return e;
     }
+
+    /**
+	 * Crea un nuevo arreglo con números pseudoaleatorios.
+	 * @param n el tamaño del arreglo a crear.
+	 * @param max el mayor elemento a generar en el arreglo.
+	 * @return un arreglo de tamaño n con números pseudoaleatorios de 0 a 19.
+	 */
+	public static int[] generate(int n, int max){
+		int[] res = new int[n];
+		Random rn = new Random();
+		for(int i = 0 ; i < n; i++)
+			res[i] = rn.nextInt(max);
+		return res;
+	}
+
+    public static void main(String[] args) {
+		int[] arr1 = generate(15, 15);
+		int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+		int[] arr3 = Arrays.copyOf(arr1, arr1.length);
+		//System.out.println("No ordenado: " + Arrays.toString(arr1));
+		
+        System.out.println("\nNo ordenado: " + Arrays.toString(arr1));
+		double inicio = System.currentTimeMillis();
+		quicksort(arr1);
+		double fin = System.currentTimeMillis();
+		System.out.println("Ordenado: " + Arrays.toString(arr1) + "\n");
+		System.out.println("Ordenado con quicksort tardó: " + (fin - inicio) + " milisegundos");
+	}
 }

280c39abd20a00262e29e01be20b14955593d7fa
ZuriUG <zuriug@ciencias.unam.mx>
2021-11-22 17:40:20 -0600

    Se añade método retrieve

--- Prácticas/Practica05RodríguezUlisesUribeZurisadai/src/fciencias/edatos/practica05/BinarySearchTree.java
+++ Prácticas/Practica05RodríguezUlisesUribeZurisadai/src/fciencias/edatos/practica05/BinarySearchTree.java
@@ -1,8 +1,8 @@
 package fciencias.edatos.practica05;
 
-public class BinarySearchTree{
+public class BinarySearchTree<K extends Comparable<K>, T> implements TDABinarySearchTree<K, T>{
     
-    public class BinaryNode<K extends Comparable<K>, T> implements TDABinarySearchTree<K, T>{
+    public class BinaryNode{
 
         /** Clave */
         public K key;
@@ -19,11 +19,47 @@ public class BinarySearchTree{
         /** Hijo derecho */
         public BinaryNode right;
 
-        public BinaryNode(k key, T element, BinaryNode, parent) {
+        public BinaryNode(K key, T element, BinaryNode parent) {
             this.key = key;
-            
+            this.element = element;
+            this.parent = parent;
         }
     }
+    /** Rama */
+    private BinaryNode root;
+
+    /**
+     * Devuelve el elemento.
+     * @param k clave k.
+     * @return el elemento asociado a dicha clave, 
+     * null si no existe.
+     */
+    @Override
+	public T retrieve(K k){
+		BinaryNode node = retrieve(root, k);
+		if(node == null)
+			return null;
+		return node.element;
+	}
+
+	private BinaryNode retrieve(BinaryNode actual, K k){
+		// Si el elemento no se encuentra.
+		if(actual == null)
+			return null;
+
+		// Si se encuentra el elemento.
+		if(actual.key.compareTo(k) == 0)
+			return actual;
+
+		// Comparamos.
+		if(k.compareTo(actual.key) < 0){ 
+			return retrieve(actual.left, k);
+		} else { 
+			return retrieve(actual.right, k);
+		}
+	}
+
+
     
     public static void main(String[] args) {
         

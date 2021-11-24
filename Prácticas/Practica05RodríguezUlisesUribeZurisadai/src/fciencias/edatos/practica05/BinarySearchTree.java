236e0b0703333d6957d1b016271359e0974e4bc3
U <ulises.rdgz@ciencias.unam.mx>
2021-11-23 21:03:22 -0600

    Agregamos método insert, isEmpty y preorden

--- Prácticas/Practica05RodríguezUlisesUribeZurisadai/src/fciencias/edatos/practica05/BinarySearchTree.java
+++ Prácticas/Practica05RodríguezUlisesUribeZurisadai/src/fciencias/edatos/practica05/BinarySearchTree.java
@@ -1,40 +1,44 @@
 package fciencias.edatos.practica05;
 
-public class BinarySearchTree<K extends Comparable<K>, T> implements TDABinarySearchTree<K, T>{
-    
-    public class BinaryNode{
+public class BinarySearchTree<K extends Comparable<K>, T> implements TDABinarySearchTree<K, T>	{
+	
+	/**
+	 * Nodo para un árbol binario de búsqueda.
+	 */
+	public class BinaryNode{
 
-        /** Clave */
-        public K key;
+		/** Clave. */
+		public K key;
 
-        /** Elemento */
-        public T element;
+		/** Elemento. */
+		public T element;
 
-        /** Padre del nodo */
-        public BinaryNode parent;
+		/** Padre del nodo. */
+		public BinaryNode parent;
 
-        /** Hijo izquierdo */
-        public BinaryNode left;
+		/** Hijo izquierdo. */
+		public BinaryNode left;
 
-        /** Hijo derecho */
-        public BinaryNode right;
+		/** Hijo derecho. */
+		public BinaryNode rigth;
 
-        public BinaryNode(K key, T element, BinaryNode parent) {
-            this.key = key;
-            this.element = element;
-            this.parent = parent;
-        }
-    }
-    /** Rama */
-    private BinaryNode root;
+		/**
+		 * Crea un nuevo nodo.
+		 * @param key la clave.
+		 * @param element el elemento a almacenar.
+		 * @param parent el padre del nodo.
+		 */
+		public BinaryNode(K key, T element, BinaryNode parent){
+			this.key = key;
+			this.element = element;
+			this.parent = parent;
+		}
+	}
 
-    /**
-     * Devuelve el elemento.
-     * @param k clave k.
-     * @return el elemento asociado a dicha clave, 
-     * null si no existe.
-     */
-    @Override
+	/** Root */
+	private BinaryNode root;
+
+	@Override
 	public T retrieve(K k){
 		BinaryNode node = retrieve(root, k);
 		if(node == null)
@@ -43,25 +47,98 @@ public class BinarySearchTree<K extends Comparable<K>, T> implements TDABinarySe
 	}
 
 	private BinaryNode retrieve(BinaryNode actual, K k){
-		// Si el elemento no se encuentra.
+		// No se encuentra el elemento
 		if(actual == null)
 			return null;
 
-		// Si se encuentra el elemento.
+		// Si encontramos el elemento
 		if(actual.key.compareTo(k) == 0)
 			return actual;
 
-		// Comparamos.
-		if(k.compareTo(actual.key) < 0){ 
+		// Comparamos los elementos
+		if(k.compareTo(actual.key) < 0){ // Verificamos en la izquierda
 			return retrieve(actual.left, k);
-		} else { 
-			return retrieve(actual.right, k);
+		} else { // Verificar en la derecha
+			return retrieve(actual.rigth, k);
 		}
 	}
 
+    @Override
+	public void insert(T e, K k){
+        if (isEmpty()){
+        	root = new BinaryNode(k, e, null);
+			return;
+		}
 
-    
-    public static void main(String[] args) {
+        BinaryNode actual = root;
+        insert(actual, e, k);
+	}
+	
+	private void insert(BinaryNode actual, T e, K k){
+
+        if(k.compareTo(actual.key) < 0){ // Verificamos en la izquierda
+			if (actual.left != null)
+                insert(actual.left, e, k);
+			else
+				actual.left = new BinaryNode(k, e, actual);
+                
+		} else { // Verificar en la derecha
+            if (actual.rigth != null)
+                insert(actual.rigth, e, k);
+			else
+				actual.rigth = new BinaryNode(k, e, actual);
+		}
+	}
+
+    @Override
+    public T delete(K k) {
+		return null;
+    }
+
+    @Override
+    public T findMin() {
+        return null;
+    }
+
+    @Override
+    public T findMax() {
+        return null;
+    }
+
+    @Override
+    public void preorden() {
+        preorden(root);
+    }
+
+    public void preorden(BinaryNode actual){
+
+		if (actual == null)
+			return;
+
+        System.out.println(actual.element);
+
+        if (actual.left != null)
+            preorden(actual.left);
+
+		if (actual.rigth != null)
+            preorden(actual.rigth);
+    }
+
+    @Override
+    public void inorden() {
         
     }
+
+    @Override
+    public void postorden() {
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (root == null)
+            return true;
+        return false;
+    }
 }
+
---
+++ Prácticas/Practica05RodríguezUlisesUribeZurisadai/src/fciencias/edatos/practica05/Main.java
@@ -0,0 +1,18 @@
+package fciencias.edatos.practica05;
+
+public class Main {
+    public static void main(String[] args) {
+        TDABinarySearchTree<Integer, String> tree = new BinarySearchTree<>();
+
+        tree.insert("A", 12);
+        tree.insert("B", 2);
+        tree.insert("C", 6);
+        tree.insert("D", 16);
+        tree.insert("E", 1);
+        tree.insert("F", 3);
+        tree.insert("G", 0);
+
+        tree.preorden();
+
+     }
+}
